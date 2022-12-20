172d80a083428cda58cff5cb19b62a55047becd8
Yafet Abebe <yafetema15@gmail.com>
2022-12-20 05:38:44 +0300

    v2.2

---
+++ app/src/main/java/com/example/ethiopiaactivity/AfarActivity.java
@@ -0,0 +1,14 @@
+package com.example.ethiopiaactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AfarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_afar);
+    }
+}
---
+++ app/src/main/java/com/example/ethiopiaactivity/SidamaActivity.java
@@ -0,0 +1,14 @@
+package com.example.ethiopiaactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SidamaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sidama);
+    }
+}
